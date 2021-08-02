87ddb0d3f096df4e2bafeb7cebe80283d12e5ca2
HuangYi <123337671@qq.com>
2021-08-02 17:32:14 +0800

    :fire: 更改部分不需要的步骤

--- Core/Src/callback.c
+++ Core/Src/callback.c
@@ -16,7 +16,6 @@ static u8  txbuf [2]    = {0};
 //-------------------------------标志---------------------------------------------
 u8 ADS8688_BUSY   = 0;     //ADS8688 DMA接收还未完成
 u8 FreMeasure_STA = 0;     //等距测量状态 0-预备 1-初次捕获 2-正在 3-过阈值 4-完成
-u32 Nu=0,Ni=0;
 float RefFrequency = 84001168.0f;
 //--------------------------------------------------------------------------------
 void TIM1_PeriodElapsedCallback(void);  //TIM1 更新ISR--用于ADS采样
@@ -25,7 +24,6 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 {
   if (htim->Instance == TIM6) HAL_IncTick(); // FreeRTOS 系统时钟
   if (htim == &htim1) TIM1_PeriodElapsedCallback();
-  if (htim == &htim3) Nu++;
   if (htim == &htim5) {
   	FreMeasure_STA=3;
   }
@@ -34,12 +32,12 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim)
 {
   static u32 CNT_First,CNT_Last;
+  static u32 Ni=0,Nu=0;
   if (htim == &htim3) {
     switch(FreMeasure_STA) {
     	case 0:{
     		__HAL_TIM_SET_COUNTER(&htim5,0);
     		__HAL_TIM_ENABLE_IT(&htim5,TIM_IT_UPDATE);
-    		__HAL_TIM_ENABLE_IT(&htim3,TIM_IT_UPDATE);
     		FreMeasure_STA = 1;
     	};break;
     	case 1:{
@@ -53,7 +51,6 @@ void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim)
     	case 3:{
     		CNT_Last = __HAL_TIM_GET_COUNTER(&htim5);
     		__HAL_TIM_DISABLE_IT(&htim5,TIM_IT_UPDATE);
-    		__HAL_TIM_DISABLE_IT(&htim3,TIM_IT_UPDATE);
     		Nu = (htim5.Instance->ARR+1) * (htim5.Instance->PSC+1) * 1 + CNT_Last - CNT_First;
     		TargetFrequency = Ni * RefFrequency / Nu;
     		Ni = Nu = 0;
